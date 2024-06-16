703c58a58243061d5732908dc0805f12716c95c5
change41 <lcy19910401@hotmail.com>
2024-06-16 13:34:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.xuuuu.top": dockerHub,
+  "quay.xuuuu.top": "https://quay.io",
+  "gcr.xuuuu.top": "https://gcr.io",
+  "k8s-gcr.xuuuu.top": "https://k8s.gcr.io",
+  "k8s.xuuuu.top": "https://registry.k8s.io",
+  "ghcr.xuuuu.top": "https://ghcr.io",
+  "cloudsmith.xuuuu.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.xuuuu.top": dockerHub,
 };
 
 function routeByHosts(host) {
